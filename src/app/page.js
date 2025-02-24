64a310446d06e7492204e2432a14c1ec102a2c6a
cosminsaracil <cosmin.saracil1@gmail.com>
2025-02-24 17:51:06 +0200

    [UI] check for possible breakpoints in themes. And implement switch for it.

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import styles from "./page.module.css";
+import Button from "@mui/material/Button";
 
 // Home page
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <h1>Hello NEXT!</h1>
+        <Button variant="contained">Click me</Button>
+      </main>
       <footer className={styles.footer}></footer>
     </div>
   );
