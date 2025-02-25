d4101e3957059b31dd86cca76cca6af0f798a513
cosminsaracil <cosmin.saracil1@gmail.com>
2025-02-25 13:46:02 +0200

    [_app] Added theme to the main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
+"use client"; // 👈 Mark this as a Client Component
 import styles from "./page.module.css";
 import Button from "@mui/material/Button";
+
+// ** Theme
+import ThemeRegistry from "@/components/theme/ThemeProvider";
 
 // Home page
 
@@ -7,8 +11,10 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Hello NEXT!</h1>
-        <Button variant="contained">Click me</Button>
+        <ThemeRegistry>
+          <h1>Hello NEXT!</h1>
+          <Button variant="contained">Click me</Button>
+        </ThemeRegistry>
       </main>
       <footer className={styles.footer}></footer>
     </div>
---
+++ src/components/theme/ThemeProvider.js
@@ -0,0 +1,17 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+export default function ThemeRegistry({ children }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+}
