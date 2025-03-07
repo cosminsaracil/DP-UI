77a5d1cb95f45c9b9dcbd1588560f0e9b9d507ff
Cosmin Sărăcil <cosmin.saracil1@gmail.com>
2025-03-07 21:38:30 +0200

    route for api

---
+++ src/app/api/hello/route.js
@@ -0,0 +1,5 @@
+// app/api/hello/route.js
+export async function GET() {
+    return Response.json({ message: "Hello from the API!" });
+  }
+  
--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,56 @@
-"use client"; // 👈 Mark this as a Client Component
-import styles from "./page.module.css";
-import Button from "@mui/material/Button";
+// "use client"; 
 
+// export default function Home() {
+  //   return (
+    //     <div className={styles.page}>
+    //       <ThemeRegistry>
+    //         <main className={styles.main}>
+    //           <h1>Hello NEXT!</h1>
+    //           <Button variant="contained">Click me</Button>
+    //         </main>
+    //         <footer className={styles.footer}></footer>
+    
+    //       </ThemeRegistry>
+    //     </div>
+    //   );
+    // }
+    
+    // src/app/page.js
+'use client'
+import { useState, useEffect } from 'react';
+import Button from "@mui/material/Button";
+import styles from "./page.module.css";
 // ** Theme
 import ThemeRegistry from "@/components/theme/ThemeProvider";
-
-// Home page
-
+    
 export default function Home() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/hello'); // ✅ This now works!
+        console.log(response)
+        const data = await response.json();
+        setMessage(data.message);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className={styles.page}>
-      <ThemeRegistry>
-        <main className={styles.main}>
-          <h1>Hello NEXT!</h1>
-          <Button variant="contained">Click me</Button>
-        </main>
+           <ThemeRegistry>
+             <main className={styles.main}>
+      <h2>Welcome to the Home Page!</h2>
+      <p>{message}</p>
+      </main>
         <footer className={styles.footer}></footer>
-      </ThemeRegistry>
-    </div>
+    
+     </ThemeRegistry>
+     </div>
   );
 }
